ee5173b5162d58710c5668d113f22136b125ec48
SurielAsh <46661069+SurielAsh@users.noreply.github.com>
2019-04-12 11:59:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main() {
     int n,Gmax,e1,e2,num;
     double w;
     //******************
-    n=8;
+    n=15;
     Gmax=10000;
     e1=3;
     e2=3;
@@ -34,29 +34,29 @@ int main() {
     {
         vptl.push_back(gbest);
     }
-    //Dprt0(vptl);
-    for(int i=0;i<num;i++) {
-    	vptl[i].ini();
-    }
+    Dprt0(vptl);
+    //for(int i=0;i<num;i++) {
+    	//vptl[i].ini();
+    //}
 
 
     for(int gen=0;gen<Gmax;gen++)
     {
-        //cout<<"第 "<<gen<<" 代,gbest适应度为"<<gbest.fit()<<endl;
+        cout<<"第 "<<gen<<" 代,gbest适应度为"<<gbest.fit()<<endl;
         for (int i = 0; i < num; i++) {
             bool b = (vptl[i].fit() <= gbest.fit());
             if (b == 1)
                 gbest.pos = vptl[i].pos;
-            vptl[i].updt(w,gbest);
+            //vptl[i].updt(w,gbest);
         }
-        //update0(vptl,w,gbest);
+        update0(vptl,w,gbest);
         fw(Gmax,gen,w);
-        map temp=map1;
-        for(int i=0;i<n;i++)
-        {
-            temp[i][gbest.pos[i]]=-1;
-        }
-        temp.shwmap();
+        //map temp=map1;
+        //for(int i=0;i<n;i++)
+        //{
+            //temp[i][gbest.pos[i]]=-1;
+        //}
+        //temp.shwmap();
         //system("clear");
     }
     for(int i=0;i<n;i++)
